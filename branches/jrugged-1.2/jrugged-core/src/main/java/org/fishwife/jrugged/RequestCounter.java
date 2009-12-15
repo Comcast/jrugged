3a2e31cee61963d9bc81a739b0283eb22638f5a2
Jon Moore <jon_moore@comcast.com>
2009-12-15 19:03:57 +0000

    Copying release tag 1.2.0 to a 1.2 bugfix branch. Bugfixes to this branch should be forward-ported to later release trains.

---
+++ branches/jrugged-1.2/jrugged-core/src/main/java/org/fishwife/jrugged/RequestCounter.java
@@ -0,0 +1,83 @@
+/* RequestCounter.java
+ * 
+ * Copyright 2009 Comcast Interactive Media, LLC.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fishwife.jrugged;
+
+import java.util.concurrent.Callable;
+
+/** This is a statistics wrapper that counts total requests, as well
+ *  as how many succeed and how many fail. This class can be polled
+ *  periodically to measure request rates, success rates, and failure
+ *  rates.
+ */
+public class RequestCounter implements ServiceWrapper {
+    private long numRequests = 0L;
+    private long numSuccesses = 0L;
+    private long numFailures = 0L;
+
+    private synchronized void succeed() {
+	numRequests++;
+	numSuccesses++;
+    }
+
+    private synchronized void fail() {
+	numRequests++;
+	numFailures++;
+    }
+
+    public <T> T invoke(Callable<T> c) throws Exception {
+	try {
+	    T result = c.call();
+	    succeed();
+	    return result;
+	} catch (Exception e) {
+	    fail();
+	    throw e;
+	}
+    }
+
+    public void invoke(Runnable r) throws Exception {
+	try {
+	    r.run();
+	    succeed();
+	} catch (Exception e) {
+	    fail();
+	    throw e;
+	}
+    }
+
+    public <T> T invoke(Runnable r, T result) throws Exception {
+	try {
+	    r.run();
+	    succeed();
+	    return result;
+	} catch (Exception e) {
+	    fail();
+	    throw e;
+	}
+    }
+
+    /** Samples the current counts.
+     *  @return an array of three <code>longs</code>: the total
+     *    number of requests, the number of successful requests,
+     *    and the number of failed requests.
+     */
+    public synchronized long[] sample() {
+	long[] out = { numRequests, numSuccesses, numFailures };
+	return out;
+    }
+
+}
---
+++ branches/jrugged-1.2/jrugged-core/src/test/java/org/fishwife/jrugged/TestInitializer.java
@@ -0,0 +1,93 @@
+/* TestInitializer.java
+ * 
+ * Copyright 2009 Comcast Interactive Media, LLC.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fishwife.jrugged;
+
+import junit.framework.TestCase;
+import static org.easymock.EasyMock.*;
+
+public class TestInitializer extends TestCase {
+    private Initializable mockClient;
+    private Initializer impl;
+
+    public void setUp() {
+	mockClient = createMock(Initializable.class);
+	impl = new Initializer(mockClient);
+    }
+
+    public void testFirstTimeTrial() throws Exception {
+	mockClient.tryInit();
+	mockClient.afterInit();
+	replay(mockClient);
+
+	impl.initialize();
+	Thread.sleep(5);
+
+	verify(mockClient);
+	assertTrue(impl.isInitialized());
+	assertFalse(impl.isCancelled());
+	assertEquals(1, impl.getNumAttempts());
+    }
+
+    public void testThirdTimesACharm() throws Exception {
+	mockClient.tryInit();
+	expectLastCall().andThrow(new RuntimeException()).times(2);
+	mockClient.tryInit();
+	mockClient.afterInit();
+	replay(mockClient);
+
+	impl.setRetryMillis(1);
+	impl.initialize();
+	Thread.sleep(5);
+
+	assertTrue(impl.isInitialized());
+	assertFalse(impl.isCancelled());
+	assertEquals(3, impl.getNumAttempts());
+	verify(mockClient);
+    }
+
+    public void testExceededRetries() throws Exception {
+	mockClient.tryInit();
+	expectLastCall().andThrow(new RuntimeException()).times(2);
+	replay(mockClient);
+
+	impl.setMaxRetries(2);
+	impl.setRetryMillis(1);
+	impl.initialize();
+	Thread.sleep(5);
+	
+	assertFalse(impl.isInitialized());
+	assertFalse(impl.isCancelled());
+	assertEquals(2, impl.getNumAttempts());
+	verify(mockClient);
+    }
+
+    public void testCancellation() throws Exception {
+	mockClient.tryInit();
+	expectLastCall().andThrow(new RuntimeException());
+	replay(mockClient);
+
+	impl.initialize();
+	Thread.sleep(1);
+	impl.destroy();
+	
+	assertFalse(impl.isInitialized());
+	assertTrue(impl.isCancelled());
+	assertEquals(1, impl.getNumAttempts());
+	verify(mockClient);
+    }
+
+}
