c47b2c2d7a3b7132fd9831035d5d43358509ae4b
David Cleaver <david_cleaver@comcast.com>
2010-01-05 15:22:26 +0000

    Tagging release 1.3.0

---
+++ tags/jrugged-1.3.0/jrugged-core/src/main/java/org/fishwife/jrugged/FlowMeter.java
@@ -0,0 +1,75 @@
+/* FlowMeter.java
+ * 
+ * Copyright 2009 Comcast Interactive Media, LLC.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fishwife.jrugged;
+
+/** This class uses a raw {@link RequestCounter} to compute request rates
+ *  over time for total rate of request, rate of successful requests, and
+ *  rate of failed requests.
+ */
+public class FlowMeter {
+    private RequestCounter counter;
+
+    private long lastTotal;
+    private long lastSuccesses;
+    private long lastFailures;
+    private long lastSampleMillis;
+
+    /** Constructs a {@link FlowMeter}.
+     *  @param counter the {@link RequestCounter} to calculate request
+     *    rates from
+     */
+    public FlowMeter(RequestCounter counter) {
+	this.counter = counter;
+    }
+
+    /** Calculates requests per second. */
+    private double rate(long events, long t) {
+	return ((double)events / (double)t) * 1000.0;
+    }
+
+    /** Takes a sample of the request rates. Calculations are based on
+     *  differences in request counts since the last call to 
+     *  <code>sample()</code>.
+     *  @return an array of three <code>doubles</code>: total requests per
+     *    second, successful requests per second, failed requests per
+     *    second. If this is the first sample, all three rates will be
+     *    reported as zero requests per second.
+     */
+    public synchronized double[] sample() {
+	long[] currCounts = counter.sample();
+	long now = System.currentTimeMillis();
+	double[] out = new double[3];
+
+	if (lastSampleMillis != 0) {
+	    long deltaTime = now - lastSampleMillis;
+
+	    out[0] = rate(currCounts[0] - lastTotal, deltaTime);
+	    out[1] = rate(currCounts[1] - lastSuccesses, deltaTime);
+	    out[2] = rate(currCounts[2] - lastFailures, deltaTime);
+
+	} else {
+	    out[0] = out[1] = out[2] = 0.0;
+	}
+	
+	lastTotal = currCounts[0];
+	lastSuccesses = currCounts[1];
+	lastFailures = currCounts[2];
+	lastSampleMillis = now;
+	
+	return out;
+    }
+}
---
+++ tags/jrugged-1.3.0/jrugged-core/src/test/java/org/fishwife/jrugged/TestLatencyTracker.java
@@ -0,0 +1,62 @@
+/* TestLatencyTracker.java
+ * 
+ * Copyright 2009 Comcast Interactive Media, LLC.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fishwife.jrugged;
+
+import java.util.concurrent.Callable;
+
+import junit.framework.TestCase;
+
+public class TestLatencyTracker extends TestCase {
+    private LatencyTracker impl;
+
+    public void setUp() { 
+	impl = new LatencyTracker(); 
+    }
+
+    public void testCallableSuccess() throws Exception {
+	final Object o = new Object();
+	
+	Object result = impl.invoke(new Callable() {
+		public Object call() throws Exception {
+		    Thread.sleep(1);
+		    return o;
+		}
+	    });
+
+	assertSame(result, o);
+	assertTrue(impl.getLastSuccessMillis() > 0);
+	assertEquals(0, impl.getLastFailureMillis());
+    }
+
+    public void testCallableFailure() throws Exception {
+	
+	try {
+	    Object result = impl.invoke(new Callable() {
+		    public Object call() throws Exception {
+			Thread.sleep(1);
+			throw new Exception();
+		    }
+		});
+	    fail("should have thrown exception");
+	} catch (Exception expected) {
+	}
+	assertTrue(impl.getLastFailureMillis() > 0);
+	assertEquals(0, impl.getLastSuccessMillis());
+    }
+
+
+}
