c9903b9a9630dc8885078d7c8112d217f44d8481
Jon Moore <jon_moore@comcast.com>
2011-02-25 15:16:37 +0000

    Adding a new interface to jrugged-core for stamping out a set of related ServiceWrappers that each can be assigned a distinct name (will be used for per-host ServiceWrapping in jrugged-httpclient).

---
+++ trunk/jrugged-core/src/main/java/org/fishwife/jrugged/ServiceWrapperFactory.java
@@ -0,0 +1,17 @@
+package org.fishwife.jrugged;
+
+/**
+ * A {@link ServiceWrapperFactory} is a way to create several related
+ * but distinct {@link ServiceWrapper} instances.
+ *
+ */
+public interface ServiceWrapperFactory {
+    
+    /**
+     * Create a new <code>ServiceWrapper</code> using the following
+     * (presumed unique) identifier.
+     * @param name to apply to the new <code>ServiceWrapper</code>
+     * @return <code>ServiceWrapper</code>
+     */
+    ServiceWrapper getWrapperWithName(String name);
+}
