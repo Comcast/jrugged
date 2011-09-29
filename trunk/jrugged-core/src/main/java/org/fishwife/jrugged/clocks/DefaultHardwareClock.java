0de437f42a79e5b056ac407c19539003055e3cdd
Jon Moore <jon_moore@comcast.com>
2011-09-29 21:39:01 +0000

    + Extracted HardwareClock interface.

--- trunk/jrugged-core/src/main/java/org/fishwife/jrugged/clocks/DefaultHardwareClock.java
+++ trunk/jrugged-core/src/main/java/org/fishwife/jrugged/clocks/DefaultHardwareClock.java
@@ -25,7 +25,7 @@ import org.fishwife.jrugged.interval.DiscreteInterval;
  * the actual granularity of the timer (which might be greater than 1ns)
  * and present the measurement error inherent in taking clock measurements.
  */
-class DefaultHardwareClock {
+class DefaultHardwareClock implements HardwareClock {
 
     private static long DEFAULT_PERIOD_MILLIS = 100 * 1000L;
     private long periodMillis = DEFAULT_PERIOD_MILLIS;
@@ -83,10 +83,8 @@ class DefaultHardwareClock {
         return elapsedTime(start, end);
     }
     
-    /** Gets the estimated hardware clock granularity. This is the
-     * number of nanoseconds that elapse between ticks/updates of
-     * the underlying hardware clock.
-     * @return granularity in nanoseconds
+    /* (non-Javadoc)
+     * @see org.fishwife.jrugged.clocks.HardwareClock#getGranularity()
      */
     public long getGranularity() {
         long now = env.currentTimeMillis();
@@ -104,14 +102,8 @@ class DefaultHardwareClock {
         return maxGranularity;
     }
     
-    /** Get an estimate of the current hardware clock reading, 
-     * represented as a range of times. The +/- error should be half
-     * of the measurement increment, which is the clock granularity
-     * in our case. Thus, this effectively returns the hardware clock
-     * reading +/- (half granularity), with conservative rounding.
-     * @return <code>DiscreteInterval</code> representing the range of values the
-     *   current hardware clock time might fall within, represented in
-     *   nanoseconds.
+    /* (non-Javadoc)
+     * @see org.fishwife.jrugged.clocks.HardwareClock#getNanoTime()
      */
     public DiscreteInterval getNanoTime() {
         long granularity = getGranularity();
