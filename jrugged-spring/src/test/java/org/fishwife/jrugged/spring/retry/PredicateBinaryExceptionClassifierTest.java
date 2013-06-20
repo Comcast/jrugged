ad0cb24a4f55f93998b225f869b8752fc3a86977
Matthew Inger <matthew_inger@cable.comcast.com>
2013-06-20 10:26:08 -0400

    Extensions to spring-batch's retry framework.
    In particular, this adds the following capability:
    
    1.  Support for the Callable class within the ExtendedRetryTemplate
    2.  A Classifier instance which is based on guava predicates
    3.  Some useful predicates
    4.  A retry policy which is based on a generic exception classifier, rather than the existing SimpleRetryPolicy which assumes a Map based approach to determining exception classification.

---
+++ jrugged-spring/src/test/java/org/fishwife/jrugged/spring/retry/PredicateBinaryExceptionClassifierTest.java
@@ -0,0 +1,35 @@
+/* Copyright 2009-2012 Comcast Interactive Media, LLC.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package org.fishwife.jrugged.spring.retry;
+
+import com.google.common.base.Predicate;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class PredicateBinaryExceptionClassifierTest {
+
+    @Test
+    public void testClassify() {
+        Predicate predicate = Mockito.mock(Predicate.class);
+        PredicateBinaryExceptionClassifier classifier = new PredicateBinaryExceptionClassifier(predicate);
+        classifier.setPredicate(predicate);
+        Mockito.when(predicate.apply(Mockito.any(Throwable.class))).thenReturn(false);
+        Assert.assertSame(predicate, classifier.getPredicate());
+        Assert.assertFalse(classifier.classify(new RuntimeException()));
+        Mockito.verify(predicate, Mockito.times(1)).apply(Mockito.any(Throwable.class));
+    }
+}
