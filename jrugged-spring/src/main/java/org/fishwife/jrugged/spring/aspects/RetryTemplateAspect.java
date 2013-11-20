67d6313e0c03b995c90a31ae2bd266e8007bf7e9
Matthew Inger <matthew_inger@cable.comcast.com>
2013-11-19 21:11:59 -0500

    Adding the RetryTemplate annotation and associated RetryTemplateAspect.
    This will allow us to annotate method calls, and lookup the requested RetryTemplate
    instance from the spring context, and enclose the method call inside the retry loop.

---
+++ jrugged-spring/src/main/java/org/fishwife/jrugged/spring/aspects/RetryTemplateAspect.java
@@ -0,0 +1,92 @@
+package org.fishwife.jrugged.spring.aspects;
+
+import com.google.common.base.Strings;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+
+@Aspect
+public class RetryTemplateAspect implements BeanFactoryAware {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(RetryTemplateAspect.class);
+
+    private BeanFactory beanFactory;
+
+    /** Default constructor. */
+    public RetryTemplateAspect() {
+    }
+
+    @Autowired
+    @Required
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    /** Runs a method call through the spring managed
+     * {@link org.springframework.retry.support.RetryTemplate} instance indicated
+     * by the annotations "name" attribute.
+     *
+     * @param pjp a {@link org.aspectj.lang.ProceedingJoinPoint} representing an annotated
+     * method call.
+     * @param retryTemplateAnnotation the {@link org.fishwife.jrugged.spring.aspects.RetryTemplate} annotation
+     * that wrapped the method.
+     * @throws Throwable if the method invocation itself or the wrapping
+     * {@link org.springframework.retry.support.RetryTemplate} throws one during execution.
+     * @return The return value from the method call.
+     */
+    @Around("@annotation(retryTemplateAnnotation)")
+    public Object retry(final ProceedingJoinPoint pjp,
+                        final RetryTemplate retryTemplateAnnotation) throws Throwable {
+        final String name = retryTemplateAnnotation.name();
+        final String recoveryCallbackName = retryTemplateAnnotation.recoveryCallbackName();
+
+        org.springframework.retry.support.RetryTemplate retryTemplate =
+            beanFactory.getBean(name, org.springframework.retry.support.RetryTemplate.class);
+
+        org.springframework.retry.RecoveryCallback recoveryCallback = null;
+        if (! Strings.isNullOrEmpty(recoveryCallbackName)) {
+            recoveryCallback =
+                    beanFactory.getBean(recoveryCallbackName, org.springframework.retry.RecoveryCallback.class);
+        }
+
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Have @RetryTemplate method with retryTemplate name {} and callback name {}, " +
+                    "wrapping call on method {} of target object {}",
+                    new Object[]{
+                            name,
+                            recoveryCallbackName,
+                            pjp.getSignature().getName(),
+                            pjp.getTarget()});
+        }
+
+        return retryTemplate.execute(
+                new RetryCallback<Object>() {
+                    public Object doWithRetry(RetryContext context) throws Exception {
+                        try {
+                            return pjp.proceed();
+                        }
+                        catch (Error e) {
+                            throw e;
+                        }
+                        catch (Exception e) {
+                            throw e;
+                        }
+                        catch (Throwable e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                recoveryCallback);
+    }
+}
